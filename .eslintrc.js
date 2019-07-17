1bf55ddb4fa3297f7ac4518a1e02e04e225613de
Farid <farid.ouachrar@gmail.com>
2019-07-17 10:42:59 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": false,
+    "es6": true
+  },
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  }
+};
